ebcc02ab8d9d22aa20308288e0710e5c9342a713
VishnuRaghvan <vishnuraghav16@gmail.com>
2018-08-13 20:54:51 +0530

    camera frgament developing

--- app/src/main/java/com/example/vishnuraghavan/attendence/dashboard/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/vishnuraghavan/attendence/dashboard/fragments/CameraFragment.kt
@@ -1,24 +1,78 @@
 package com.example.vishnuraghavan.attendence.dashboard.fragments
 
+import android.Manifest
+import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
 import com.example.vishnuraghavan.attendence.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import org.jetbrains.anko.toast
 
-class CameraFragment: Fragment(){
+class CameraFragment : Fragment() {
+
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.camera_fragment,container,false)
+        return inflater.inflate(R.layout.camera_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        super.onViewCreated(view, savedInstanceState)
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+        val activity = requireActivity()
+
+        Dexter.withActivity(activity)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?, token: PermissionToken?) {
+
+                    }
+
+                })
+                .check()
+
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                context!!.toast("The decoded Content is : ${it.text}")
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+
     }
 
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
 
 
 }
